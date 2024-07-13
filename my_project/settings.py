77ee692df35ec324e4b52436ed0d5bd682dd8217
Cristina Becheru <cristina_becheru@yahoo.com>
2024-07-13 14:37:35 +0000

    Challange

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e!%!u*-1mr&ylej^+v87-wvz3-)80y2y8ot&t3w3zvdw-^l5s0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','.herokuapp.com']
 
